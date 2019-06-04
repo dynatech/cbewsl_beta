5534b4e907d93b4e4dc14baab0c2d6145312aa3d
jhndvdgvr <david063095@gmail.com>
2019-06-04 08:55:47 +0800

    +added surficial implem

--- app/components/Dashboard/surficial_data/summary.js
+++ app/components/Dashboard/surficial_data/summary.js
@@ -5,6 +5,8 @@ import { ScrollView } from 'react-native-gesture-handler';
 import PureChart from 'react-native-pure-chart';
 import { defaults } from '../../../assets/styles/default_styles'
 import Storage from '../../utils/storage'
+import { NavigationEvents } from 'react-navigation';
+import moment from "moment"
 
 export default class Summary extends Component {
   constructor(props) {
@@ -31,48 +33,63 @@ export default class Summary extends Component {
     }
   }
 
+  formatDateTime(date = null) {
+    let timestamp = date
+    let current_timestamp = ""
+    let text_format_timestamp = ""
+    if (timestamp == null) {
+      current_timestamp = moment(new Date()).format("YYYY-MM-DD HH:MM:SS")
+      date = moment(new Date()).format("MMMM D, YYYY")
+      time = moment(new Date()).format("h:mm A")
+      text_format_timestamp = moment(new Date()).format("MMMM D, YYYY h:mm A")
+    } else {
+      current_timestamp = moment(date).format("YYYY-MM-DD HH:MM:SS")
+      date = moment(date).format("MMMM D, YYYY")
+      time = moment(date).format("h:mm A")
+      text_format_timestamp = moment(date).format("MMMM D, YYYY h:mm A")
+    }
+
+
+    return {
+      current_timestamp: current_timestamp,
+      date: date,
+      time: time,
+      text_format_timestamp: text_format_timestamp
+    }
+  }
+
   analyzeSurficialSummary(surficial_data) {
     let data_timestamp = surficial_data[0].data[0].x
-    this.setState({ surficial_summary: "Last surficial data received is on " + data_timestamp })
+    let formatted_timestamp = this.formatDateTime(data_timestamp)
+    this.setState({ surficial_summary: "Last surficial data received is on " + formatted_timestamp["text_format_timestamp"] })
   }
 
   componentDidMount() {
-    let r_a_family_risk = Storage.getItem("SurficialDataMeasurements");
-    r_a_family_risk.then(response => {
-      let line_colors = ['#7cb5ec', '#000000', '#8ce77d']
-      if (response == null) {
-        fetch('http://192.168.150.191:5000/api/surficial_data/get_surficial_data').then((response) => response.json())
-          .then((responseJson) => {
-            console.log("Initialize surficial data measurements.")
-            Storage.setItem("SurficialDataMeasurements", responseJson);
-            let surficial_data = []
-            for (const [index, value] of responseJson.entries()) {
-              let data = {
-                seriesName: value.series_name,
-                data: value.data,
-                color: line_colors[index]
-              }
-              surficial_data.push(data)
-            }
-            this.setState({ surficial_data: surficial_data })
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      } else {
+
+    let line_colors = ['#7cb5ec', '#000000', '#8ce77d']
+    fetch('http://192.168.150.191:5000/api/surficial_data/get_surficial_data').then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson)
         let surficial_data = []
-        for (const [index, value] of response.entries()) {
+        let to_local_data = []
+        let counter = 0
+        for (const [index, value] of responseJson.entries()) {
           let data = {
             seriesName: value.series_name,
             data: value.data,
             color: line_colors[index]
           }
           surficial_data.push(data)
+          // to_local_data.push()
         }
+
+        this.analyzeSurficialSummary(surficial_data)
         this.setState({ surficial_data: surficial_data })
-      }
-      this.analyzeSurficialSummary(this.state.surficial_data)
-    })
+        console.log(surficial_data)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   render() {
